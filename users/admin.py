a9ba3ca4844a5f139190e1719b54c2bf5f9d0acb
barta3 <baertschi.adrian@gmail.com>
2013-10-18 11:15:15 +0200

    Remove users app

--- users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from users.models import User
-
-# class UserAdmin(admin.ModelAdmin):
-#     fiels = ['firstName', 'lastName', 'email', 'dateOfBirth']
-
-admin.site.register(User)
--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from users import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view()),
-    url(r'^(?P<user_id>\d+)/$', views.detail)
-)
