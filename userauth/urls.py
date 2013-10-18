74e0d0a88a47b6d7910891e28d3c8a2b44b93ef6
barta3 <baertschi.adrian@gmail.com>
2013-10-18 11:43:49 +0200

    User registration

--- userauth/urls.py
+++ userauth/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('django.contrib.auth.views',
     url(r'^login/$', 'login', {'template_name': 'userauth/login.html'},
         name='userauth_login'),
-    url(r'^logout/$', 'logout', {'next_page': 'home/'},
+    url(r'^logout/$', 'logout', {'next_page': '/home'},
         name='userauth_logout'),
     url(r'^password-change/$', 'password_change',
         {'template_name': 'userauth/password_change_form.html'},
@@ -11,4 +12,12 @@ urlpatterns = patterns('django.contrib.auth.views',
     url(r'^password_change_done/$', 'password_change_done',
         {'template_name': 'userauth/password_change_done.html'},
         name='userauth_password_change_done')
+)
+urlpatterns += patterns('',
+    url(r'^registrieren/$', 'userauth.views.register',
+        {'next_page_name': 'userauth_register_done'},
+        name='userauth_register'),
+    url(r'^welcome/$',
+        TemplateView.as_view(template_name='userauth/register_done.html'),
+        name='userauth_register_done')
 )
