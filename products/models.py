3ab9aac6878a7dcc1d8d897af17c73d2241b808d
barta3 <baertschi.adrian@gmail.com>
2014-01-14 22:20:03 +0100

    extend field size

--- products/models.py
+++ products/models.py
@@ -8,8 +8,8 @@ class ProductCategory(Model):
         return self.name
 
 class Product(Model):
-    name = models.CharField(max_length=20)
-    desc = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=1000)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(upload_to='prod_images/', default='no_image.gif')
     category = models.ForeignKey(ProductCategory)
